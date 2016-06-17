52536c45120f5ebda6152a0ba09cdb098e287c9b
dotansimha <dotansimha@gmail.com>
2016-06-17 19:21:50 +0300

    Step 5.2: Added the template loading and context

--- client/imports/components/list-show.component.ts
+++ client/imports/components/list-show.component.ts
@@ -1,11 +1,20 @@
 import {Component} from "@angular/core";
 import {RouteParams} from "@angular/router-deprecated";
+import {BlazeTemplate} from "angular2-blaze-template";
 
 @Component({
-  template: 'Hello ListShow!'
+  directives: [BlazeTemplate],
+  template: `<blaze-template name="App_body" [context]="templateContext"></blaze-template>`
 })
 export class ListShowComponent {
+  private templateContext : any;
+
   constructor(params : RouteParams) {
     let listId = params.get("_id");
+
+    this.templateContext = {
+      main: "Lists_show_page",
+      _id: listId
+    };
   }
 }
