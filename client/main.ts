d7aaa46059bf4958967e7a00f055b79de8d32af2
dotansimha <dotansimha@gmail.com>
2016-06-17 18:51:58 +0300

    Step 3.2: Added routes definitions using Angular 2 router

--- client/main.ts
+++ client/main.ts
@@ -2,13 +2,19 @@ import '/imports/startup/client';
 
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from "@angular/router-deprecated";
 
 @Component({
-  selector: 'app',
-  template: ''
+  selector: "app",
+  directives: [ROUTER_DIRECTIVES],
+  template: `<router-outlet></router-outlet>`,
 })
+@RouteConfig([
+  {path: '/', name: 'Home', component: MainContainerComponent },
+  {path: '/lists/:_id', name: 'ListShow', component: ListShowComponent }
+])
 class MainComponent {
   constructor() {}
 }
 
-bootstrap(MainComponent, []);
+bootstrap(MainComponent, [ROUTER_PROVIDERS]);
