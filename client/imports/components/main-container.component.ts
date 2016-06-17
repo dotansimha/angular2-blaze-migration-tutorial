878fa9a4fa433d4889f6d504e6ff07fc9d378e54
dotansimha <dotansimha@gmail.com>
2016-06-17 19:10:45 +0300

    Step 4.2: Added router redirection

--- client/imports/components/main-container.component.ts
+++ client/imports/components/main-container.component.ts
@@ -1,11 +1,16 @@
 import {Component} from "@angular/core";
 import {MeteorComponent} from "angular2-meteor";
+import {Lists} from "../../../imports/api/lists/lists.js";
+import {Router} from "@angular/router-deprecated";
 
 @Component({
   directives: [],
   template: ''
 })
 export class MainContainerComponent extends MeteorComponent {
-  constructor() {
+  constructor(router : Router) {
+    if (Lists.findOne()) {
+      router.navigate(['ListShow', {_id: Lists.findOne()._id}]);
+    }
   }
 }
