3e1c84b590de8debf81a2e2f973a144e1226628b
dotansimha <dotansimha@gmail.com>
2016-06-17 18:41:23 +0300

    Step 2.1: Create the MainComponent

--- client/main.ts
+++ client/main.ts
@@ -1,1 +1,14 @@
 import '/imports/startup/client';
+
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app',
+  template: ''
+})
+class MainComponent {
+  constructor() {}
+}
+
+bootstrap(MainComponent, []);
