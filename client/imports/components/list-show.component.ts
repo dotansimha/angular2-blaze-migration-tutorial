4ed93f5c7d298c15aab0fa8a8da2a4f9dc87e5b5
dotansimha <dotansimha@gmail.com>
2016-06-17 19:18:39 +0300

    Step 5.1: Get the ID of the list from the router

--- client/imports/components/list-show.component.ts
+++ client/imports/components/list-show.component.ts
@@ -1,8 +1,11 @@
 import {Component} from "@angular/core";
+import {RouteParams} from "@angular/router-deprecated";
 
 @Component({
   template: 'Hello ListShow!'
 })
 export class ListShowComponent {
-
+  constructor(params : RouteParams) {
+    let listId = params.get("_id");
+  }
 }
