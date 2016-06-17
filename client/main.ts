f2c8c5391c143dcf41890287430483de43e8a690
dotansimha <dotansimha@gmail.com>
2016-06-17 18:57:58 +0300

    Step 3.6: Added imports

--- client/main.ts
+++ client/main.ts
@@ -3,6 +3,8 @@ import '/imports/startup/client';
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from "@angular/router-deprecated";
+import {MainContainerComponent} from "./imports/components/main-container.component";
+import {ListShowComponent} from "./imports/components/list-show.component";
 
 @Component({
   selector: "app",
