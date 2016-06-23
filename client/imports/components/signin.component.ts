b0701f44408f34b6b07416b971e8d2c424925851
dotansimha <dotansimha@gmail.com>
2016-06-23 17:00:30 +0300

    Step 7.9: Added sign in component login

--- client/imports/components/signin.component.ts
+++ client/imports/components/signin.component.ts
@@ -1,12 +1,42 @@
 import {Component} from "@angular/core";
 import {MeteorComponent} from "angular2-meteor/build/index";
+import {Router} from "@angular/router-deprecated";
+
+class SigninModel {
+  constructor(public email : string, public password : string) {
+
+  }
+}
 
 @Component({
   directives: [],
-  template: 'Signin!'
+  templateUrl: '/client/imports/components/signin.ng2.html'
 })
 export class SigninComponent extends MeteorComponent {
-  constructor() {
+  private model : SigninModel;
+  private errors : Array<string> = [];
+
+  constructor(private router : Router) {
     super();
+
+    this.model = new SigninModel('', '');
+  }
+
+  resetErrors() {
+    this.errors = [];
+  }
+
+  join() {
+    this.resetErrors();
+
+    Meteor.loginWithPassword(this.model.email, this.model.password, (err) => {
+      if (err) {
+        this.errors.push(err.reason);
+
+        return;
+      }
+
+      this.router.root.navigate(['Home']);
+    });
   }
 }
