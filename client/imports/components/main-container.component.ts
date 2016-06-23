efaba8e9e1ff6a88bb0bb93f5ec474add3346143
dotansimha <dotansimha@gmail.com>
2016-06-23 10:58:24 +0300

    Step 6.3: Added the list of todo lists

--- client/imports/components/main-container.component.ts
+++ client/imports/components/main-container.component.ts
@@ -2,23 +2,27 @@ import {Component} from "@angular/core";
 import {MeteorComponent} from "angular2-meteor";
 import {Lists} from "../../../imports/api/lists/lists.js";
 import {Router, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
+import {Mongo} from "meteor/mongo";
 
 @Component({
   directives: [ROUTER_DIRECTIVES],
   templateUrl: '/client/imports/components/main-container.ng2.html'
 })
 export class MainContainerComponent extends MeteorComponent {
-  constructor(router : Router) {
+  private lists : Mongo.Cursor;
+
+  constructor() {
     super();
 
     this.subscribe('lists.public');
     this.subscribe('lists.private');
 
-    /*this.autorun(() => {
-      if (Lists.findOne()) {
-        router.navigate(['ListShow', {_id: Lists.findOne()._id}]);
-      }
-    });*/
+    this.autorun(() => {
+      this.lists = Lists.find({ $or: [
+        { userId: { $exists: false } },
+        { userId: Meteor.userId() },
+      ] });
+    }, true);
   }
 
   isConnected() {
