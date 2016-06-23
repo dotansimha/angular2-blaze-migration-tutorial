a6f4a1e28b28d720cd3987148257c09490c1f4c6
dotansimha <dotansimha@gmail.com>
2016-06-23 16:47:41 +0300

    Step 7.4: Added the logic for join component

--- client/imports/components/join.component.ts
+++ client/imports/components/join.component.ts
@@ -1,12 +1,52 @@
 import {Component} from "@angular/core";
 import {MeteorComponent} from "angular2-meteor/build/index";
+import {Accounts} from "meteor/accounts-base";
+import {Router} from "@angular/router-deprecated";
+
+class JoinModel {
+  constructor(public email : string, public password : string, public passwordVerify : string) {
+
+  }
+}
 
 @Component({
   directives: [],
   templateUrl: '/client/imports/components/join.ng2.html'
 })
 export class JoinComponent extends MeteorComponent {
-  constructor() {
+  private model : JoinModel;
+  private errors : Array<string> = [];
+
+  constructor(private router : Router) {
     super();
+
+    this.model = new JoinModel('', '', '');
+  }
+
+  resetErrors() {
+    this.errors = [];
+  }
+
+  join() {
+    this.resetErrors();
+
+    if (this.model.password !== this.model.passwordVerify) {
+      this.errors.push("Passwords does not match!");
+
+      return;
+    }
+
+    Accounts.createUser({
+      email: this.model.email,
+      password: this.model.email
+    }, (err) => {
+      if (err) {
+        this.errors.push(err.reason);
+
+        return;
+      }
+
+      this.router.root.navigate(['Home']);
+    });
   }
 }
