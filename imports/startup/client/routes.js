2e82e96916c5a0f60f4887d24489865b6cec3802
dotansimha <dotansimha@gmail.com>
2016-06-30 13:00:22 +0300

    Step 10.6: Removed old startup files

--- imports/startup/client/routes.js
+++
@@ -1,5 +0,0 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-
-// Import to load these templates
-import '../../ui/pages/app-not-found.js';
