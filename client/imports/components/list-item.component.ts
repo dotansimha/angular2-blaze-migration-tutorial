83997cb76cbf225325bc5cb1a1431f503daa3020
dotansimha <dotansimha@gmail.com>
2016-06-30 12:16:26 +0300

    Step 9.1: Add the todo item component with migrated code

---
+++ client/imports/components/list-item.component.ts
@@ -0,0 +1,53 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+import {
+  setCheckedStatus,
+  updateText,
+  remove,
+} from '../../../imports/api/todos/methods.js';
+
+import {displayError} from '../../../imports/ui/lib/errors.js';
+
+@Component({
+  selector: 'list-item',
+  templateUrl: '/client/imports/components/list-item.ng2.html'
+})
+export class ListItem {
+  @Input() todo:any;
+  @Input() editing:boolean;
+  @Output() editChange = new EventEmitter();
+
+  constructor() {
+  }
+
+  toggleEdit(isEdit) {
+    if (!isEdit || (isEdit && this.editing)) {
+      this.editChange.emit({
+        editing: isEdit,
+        todoId: this.todo._id
+      });
+    }
+  }
+
+  remove() {
+    remove.call({
+      todoId: this.todo._id,
+    }, displayError);
+  }
+
+  checkedChange(event) {
+    setCheckedStatus.call({
+      todoId: this.todo._id,
+      newCheckedStatus: event.target.checked,
+    });
+  }
+
+  updateText(event) {
+    _.throttle((event) => {
+      updateText.call({
+        todoId: this.todo._id,
+        newText: event.target.value,
+      }, displayError);
+    }, 300)(event);
+  }
+}
