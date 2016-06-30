ad5448554c23db2611f07ae96f77b82a55faeab1
dotansimha <dotansimha@gmail.com>
2016-06-30 11:05:25 +0300

    Step 8.5: Remove the old template code

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,5 +2,5 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import to load these templates
-import '../../ui/pages/lists-show-page.js';
 import '../../ui/pages/app-not-found.js';
+import '../../ui/components/todos-item.js';
