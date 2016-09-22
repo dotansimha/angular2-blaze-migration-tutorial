3139d3c0bcd52be2117b0c0e44a7b9f45b56f564
dotansimha <dotansimha@gmail.com>
2016-09-22 15:25:54 +0300

    Step 8.3: Added list redirector route as default route

--- client/imports/app.routes.ts
+++ client/imports/app.routes.ts
@@ -1,8 +1,10 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ListShowComponent} from "./components/list-show.component";
+import {ListRedirectorComponent} from "./components/list-redirector.component";
 
 const appRoutes: Routes = [
+    {path: '', component: ListRedirectorComponent},
     {path: 'lists/:_id', component: ListShowComponent}
 ];
 
