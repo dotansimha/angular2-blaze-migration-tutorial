f8285ab064fd8c26f84a159e9f1ab58854e14cd3
dotansimha <dotansimha@gmail.com>
2016-09-22 15:25:12 +0300

    Step 8.2: Added list redirector for the module

--- client/imports/app.module.ts
+++ client/imports/app.module.ts
@@ -3,12 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {MainComponent} from "./main.component";
 import {routing} from "./app.routes";
 import {ListShowComponent} from "./components/list-show.component";
+import {ListRedirectorComponent} from "./components/list-redirector.component";
 
 @NgModule({
     // Components, Pipes, Directive
     declarations: [
         MainComponent,
-        ListShowComponent
+        ListShowComponent,
+        ListRedirectorComponent
     ],
     // Entry Components
     entryComponents: [
