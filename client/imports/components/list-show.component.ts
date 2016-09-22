9ad6c41c21cc839af844f19f794ff3df50ba54af
dotansimha <dotansimha@gmail.com>
2016-09-22 15:30:58 +0300

    Step 9.1: Get the ID of the list from the router

--- client/imports/components/list-show.component.ts
+++ client/imports/components/list-show.component.ts
@@ -1,8 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
     template: 'Hello ListShow!'
 })
-export class ListShowComponent {
+export class ListShowComponent implements OnInit {
+    constructor(private currentRoute: ActivatedRoute) {
 
+    }
+
+    ngOnInit() {
+        this.currentRoute.params.subscribe((params: Params) => {
+            const listId = params['_id'];
+        })
+    }
 }
