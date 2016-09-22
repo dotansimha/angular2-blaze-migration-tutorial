dac5a0c183dd6d54d27aef897622ee0e96164010
dotansimha <dotansimha@gmail.com>
2016-09-22 19:38:11 +0300

    Step 7.10: Implemented the signin logic

--- client/imports/components/signin.component.ts
+++ client/imports/components/signin.component.ts
@@ -1,7 +1,41 @@
-import {Component} from "@angular/core";
+import {Component, NgZone} from "@angular/core";
+import {Router} from "@angular/router";
+
+class SigninModel {
+  constructor(public email : string, public password : string) {
+
+  }
+}
 
 @Component({
-  template: 'Signin!'
+  templateUrl: '/client/imports/components/signin.html'
 })
 export class SigninComponent {
+  private model : SigninModel;
+  private errors : Array<string> = [];
+
+  constructor(private router: Router, private ngZone: NgZone) {
+    this.model = new SigninModel('', '');
+  }
+
+
+  resetErrors() {
+    this.errors = [];
+  }
+
+  join() {
+    this.resetErrors();
+
+    Meteor.loginWithPassword(this.model.email, this.model.password, (err) => {
+      if (err) {
+        this.ngZone.run(() => {
+          this.errors.push(err.reason);
+        });
+
+        return;
+      }
+
+      this.router.navigate(['/']);
+    });
+  }
 }
