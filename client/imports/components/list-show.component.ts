b34d3eabcd80868a06fc79204003be9811b525bd
dotansimha <dotansimha@gmail.com>
2016-09-22 19:43:26 +0300

    Step 8.2: Added logic for the list show component

--- client/imports/components/list-show.component.ts
+++ client/imports/components/list-show.component.ts
@@ -1,11 +1,16 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
+import {MeteorObservable} from "meteor-rxjs";
+import {Lists} from "../../../imports/api/lists/lists";
 
 @Component({
     template: '<blaze-template *ngIf="templateContext" name="Lists_show_page" [context]="templateContext"></blaze-template>'
 })
 export class ListShowComponent implements OnInit {
-    private templateContext: any;
+    private list : any;
+    private todosReady : boolean = false;
+    private todos : Array<any>;
+    private editing : boolean = false;
 
     constructor(private currentRoute: ActivatedRoute) {
 
@@ -14,10 +19,25 @@ export class ListShowComponent implements OnInit {
     ngOnInit() {
         this.currentRoute.params.subscribe((params: Params) => {
             const listId = params['_id'];
+            MeteorObservable.subscribe('todos.inList', listId).subscribe();
 
-            this.templateContext = {
-                _id: listId
-            };
+            MeteorObservable.autorun().zone().subscribe(() => {
+                if (listId && Lists.findOne(listId)) {
+                    this.list = Lists.findOne(listId);
+                    this.todosReady = true;
+                    this.todos = this.list.todos();
+                }
+            })
         });
+    }
+
+    getContextForItem(todo) {
+        return {
+              todo: todo,
+              editing: false,
+              onEditingChange(editing) {
+
+              },
+        }
     }
 }
