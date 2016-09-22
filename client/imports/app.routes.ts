66c8506097998fe4aa4d3a47be2ee5f60a650d8e
dotansimha <dotansimha@gmail.com>
2016-09-22 17:33:42 +0300

    Step 10.11: Added join/signin component to the router

--- client/imports/app.routes.ts
+++ client/imports/app.routes.ts
@@ -2,10 +2,14 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ListShowComponent} from "./components/list-show.component";
 import {ListRedirectorComponent} from "./components/list-redirector.component";
+import {JoinComponent} from "./components/join.component";
+import {SigninComponent} from "./components/signin.component";
 
 const appRoutes: Routes = [
     {path: '', component: ListRedirectorComponent},
-    {path: 'lists/:_id', component: ListShowComponent}
+    {path: 'lists/:_id', component: ListShowComponent},
+    {path: 'join', component: JoinComponent},
+    {path: 'signin', component: SigninComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
