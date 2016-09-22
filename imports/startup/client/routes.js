46f1d7cd1ec28ede9c155de4a58ae00d88a37e50
dotansimha <dotansimha@gmail.com>
2016-09-22 18:26:58 +0300

    Step 6.17: Removed App_body and root_director templates

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,8 +3,6 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 // Import to load these templates
-import '../../ui/layouts/app-body.js';
-import '../../ui/pages/root-redirector.js';
 import '../../ui/pages/lists-show-page.js';
 import '../../ui/pages/app-not-found.js';
 
