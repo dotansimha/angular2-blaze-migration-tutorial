bb81a5079cb6ae87b7cdf98e7a84844a911c6289
dotansimha <dotansimha@gmail.com>
2016-09-22 15:34:52 +0300

    Step 6.2: Use the new template and added stub methods

--- client/imports/main.component.ts
+++ client/imports/main.component.ts
@@ -1,18 +1,31 @@
 import '/imports/startup/client';
 import {Component, OnInit} from "@angular/core";
 import {MeteorObservable} from "meteor-rxjs";
+import {Meteor} from "meteor/meteor";
 
 @Component({
   selector: 'app',
-  template: '<router-outlet></router-outlet>'
+  templateUrl: '/client/imports/main-component.html'
 })
 export class MainComponent implements OnInit {
-  constructor() {
+  private isCordova : boolean;
+  private menuOpen : boolean = false;
+  private userMenuOpen : boolean = false;
 
+  constructor() {
+    this.isCordova = Meteor.isCordova;
   }
 
   ngOnInit() {
     MeteorObservable.subscribe("lists.public").subscribe();
     MeteorObservable.subscribe("lists.private").subscribe();
+  }
+
+  isConnected() {
+    return true;
+  }
+
+  get userEmail() {
+    return "";
   }
 }
