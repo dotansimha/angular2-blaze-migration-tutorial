9dc2a5c5e5cbcf2f071bec4a0eca7a5ba37dd21a
dotansimha <dotansimha@gmail.com>
2016-09-22 15:28:47 +0300

    Step 8.4: Added data subscription on the main component

--- client/imports/main.component.ts
+++ client/imports/main.component.ts
@@ -1,9 +1,18 @@
 import '/imports/startup/client';
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {MeteorObservable} from "meteor-rxjs";
 
 @Component({
   selector: 'app',
   template: '<router-outlet></router-outlet>'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    MeteorObservable.subscribe("lists.public").subscribe();
+    MeteorObservable.subscribe("lists.private").subscribe();
+  }
 }
